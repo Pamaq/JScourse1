8345950878b1e0b97180357e388b864a44e35d62
Pamaq <paulinakuta00@gmail.com>
2022-08-19 00:18:29 +0200

    Added debugging exercise

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 console.log("hello!");
+/zadanie stringi/;
 const name = "Paula";
 const petName = "Ruby";
 const petAge = 2;
 const favoriteFood = "sushi";
 console.log(`Czesc, jestem ${name}, a to jest ${petName} - mój królik.`);
 console.log(petName + " ma już " + petAge + " lat/a!");
+
+/zadanie kod/;
+let firstName = "Tomek";
+let age = 12;
+age = 18;
+
+let favColor = "niebieski";
+const favMeal = "schabowy";
+
+let currentCar;
+currentCar = "Audi";
+
+let firstName2 = "Ania";
+let age2 = 24;
+let favColor2 = "czerwony";
